bcaab8b2187a8a361bf3a3f0941716eba078f1af
Chris Tokita <christopher.tokita@gmail.com>
2017-06-22 22:02:12 -0500

    more figures

--- NetworkModel-R/scripts/Demonstration/ThresholdFunctionPlots.R
+++ NetworkModel-R/scripts/Demonstration/ThresholdFunctionPlots.R
@@ -31,11 +31,11 @@ gg_thresh <- ggplot(data = threshStyles, aes(x = stim, y = prob, colour = eta)) 
   xlab(expression(s[j])) +
   ylab(expression(P[ij])) +
   scale_y_continuous(limit = c(0, 1), breaks = seq(0, 1, 0.5), expand = c(0.1, 0)) +
-  scale_colour_manual(values = c("black", "#3c7caa", "#a0c4de"), 
+  scale_colour_manual(values = c("#08519c", "#4292c6", "#9ecae1"), 
                       labels = c(expression(paste(eta, " = 2")),
-                                 expression(paste(eta, " = 5")),
+                                 expression(paste(eta, " = 7")),
                                  expression(paste(eta, " = 20")))) +
-  annotate("text", x = 12, y = 0.0, label = threshold, parse = TRUE, size = 3) +
+  annotate("text", x = 12, y = 0.0, label = threshold, parse = TRUE, size = 3.5) +
   theme(panel.grid = element_blank(),
         panel.border = element_rect(fill = NA, size = 1),
         legend.position = c(0.96, 0.30),
@@ -76,15 +76,22 @@ sigmas$sigma <- factor(sigmas$sigma, levels = c("sigma == 0.05", "sigma == 0.1",
 
 gg_sig <- ggplot(data = sigmas, aes(x = x, y = y, group = sigma)) +
   geom_line(colour = "#74a9cf") +
-  geom_polygon(aes(x, y),  fill = "#74a9cf") +
-  theme_classic() +
+  theme_bw() +
   scale_y_continuous(expand = c(0,0), limits = c(0, 0.85)) +
   xlab(expression(theta[j])) +
   ylab("Frequency") +
-  theme(axis.text.x = element_text(size = 8),
-        axis.title = element_text(size = 10),
-        strip.text = element_text(size = 8, face = "italic"),
-        strip.background = element_rect(fill = NA, colour = NA)) +
+  panel.grid = element_blank(),
+panel.border = element_rect(fill = NA, size = 1),
+legend.position = c(0.96, 0.30),
+legend.justification = c(1, 1),
+legend.title = element_blank(),
+legend.key.height = unit(0.3, "cm"),
+legend.key.width= unit(0.4, "cm"),
+legend.margin =  margin(t = 0, r = 0, b = 0, l = -0.1, "cm"),
+legend.text = element_text(size = 6),
+legend.text.align = 0,
+axis.text.x = element_text(size = 8),
+axis.title = element_text(size = 10)) +
   facet_grid(. ~ sigma, 
              labeller = label_parsed,
              scales = "free")
